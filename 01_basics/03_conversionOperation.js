afb53994b621127015241bc1adcdfa13f2f8e51e
Prayag Raj Singh <prayagraj050@gmail.com>
2024-06-11 13:33:23 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "prayag"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//  "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "prayag"
+
+let booleaIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleaIsLoggedIn);
+
+// 1 =>true; 0 => false
+// "" => false
+// "prayag" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
